b2478953589c989f6aa3df7e6b46f99f33356098
Matevz <semprimoznik.matevz@gmail.com>
2024-05-19 17:00:13 +0200

    fix: navigation

--- src/client/src/components/ui/navigation-bar.tsx
+++ src/client/src/components/ui/navigation-bar.tsx
@@ -1,11 +1,21 @@
+'use client';
 import React from 'react';
 import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export default function NavigationBar() {
+  const pathname = usePathname();
+  const getDefault = () => {
+    return pathname.replace('/', '');
+  };
+
   return (
-    <Tabs defaultValue="map" className="fixed bottom-14 left-1/2 h-16 w-[600px] -translate-x-1/2">
-      <TabsList className="flex h-full w-full ">
+    <Tabs
+      defaultValue={getDefault()}
+      className="fixed bottom-14 left-1/2 h-16 w-[600px] -translate-x-1/2"
+    >
+      <TabsList className="flex h-full w-full">
         <Link href="/map" className="h-full w-full  p-0.5">
           <TabsTrigger className="h-full w-full" value="map">
             Map
