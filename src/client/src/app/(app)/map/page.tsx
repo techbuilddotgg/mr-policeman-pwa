80c5a86733f3c51b5aa1eab7e66e2effda0f8be4
sulcer <gregor.sulcer@student.um.si>
2024-05-22 10:52:32 +0200

    fix: remove unused imports

--- src/client/src/app/(app)/map/page.tsx
+++ src/client/src/app/(app)/map/page.tsx
@@ -1,6 +1,6 @@
 'use client';
-import React, { useEffect, useState } from 'react';
-import Map, { Marker } from 'react-map-gl';
+import React, { useState } from 'react';
+import Map from 'react-map-gl';
 import AdvancedMarker from '@/components/ui/advanced-marker';
 import Modal from '@/components/ui/modal';
 
